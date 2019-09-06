b048c6493207e31c4e1ada93114138d68a2b8bac
Eric de Alencar <ericdealencar@hotmail.com>
2019-09-06 19:41:50 -0300

    Implementado a classe adapter da Lista

---
+++ app/src/main/java/com/example/firstapp/stdio/weatherapp/fragments/WeekWeatherAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.firstapp.stdio.weatherapp.fragments
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapp.stdio.weatherapp.R
+import com.example.firstapp.stdio.weatherapp.model.WeatherModel
+import java.util.ArrayList
+
+class WeekWeatherAdapter(private var weatherList: ArrayList<WeatherModel>) :
+    RecyclerView.Adapter<WeekWeatherAdapter.WeatherHolder>() {
+    override fun onBindViewHolder(holder: WeatherHolder, position: Int) {
+        holder.bind(weatherList.get(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherHolder {
+        var view =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_item_weather, parent, false)
+        return WeatherHolder(
+            view,
+            view.findViewById(R.id.textViewTitle),
+            view.findViewById(R.id.imageViewForecastWeek),
+            view.findViewById(R.id.textViewForecastWeek),
+            view.findViewById(R.id.textViewMaxTempWeek),
+            view.findViewById(R.id.textViewMinTempWeek)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return weatherList.size
+    }
+
+
+    class WeatherHolder(
+        var itemView: View,
+        private var textViewTitle: TextView,
+        var imageViewForecastWeek: ImageView,
+        var textViewForecastWeek: TextView,
+        var textViewMaxTempWeek: TextView,
+        var textViewMinTempWeek: TextView
+    ) : RecyclerView.ViewHolder(itemView) {
+        fun bind(model: WeatherModel) {
+            textViewTitle.setText(model.title)
+            textViewForecastWeek.setText(model.forecast)
+            textViewMaxTempWeek.setText(Integer.valueOf(model.max_temp.toString()))
+            textViewMinTempWeek.setText(Integer.valueOf(model.min_tempo.toString()))
+        }
+    }
+
+}
