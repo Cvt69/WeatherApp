d81e9d372551700c3860cf90efa6ee29524307ed
Eric de Alencar <ericdealencar@hotmail.com>
2019-09-06 02:37:57 -0300

    Arquitetura de UI

--- app/src/main/java/com/example/firstapp/stdio/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/stdio/weatherapp/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.firstapp.stdio.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import androidx.viewpager.widget.ViewPager
+import com.example.firstapp.stdio.weatherapp.fragments.FragmentPageAdapter
+import com.example.firstapp.stdio.weatherapp.fragments.TodayFragment
+import com.example.firstapp.stdio.weatherapp.fragments.WeekFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: FragmentPageAdapter
+    private lateinit var tab_layout: TableLayout
+    private lateinit var viewpager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.viewpager = findViewById(R.id.viewpager)
+        //this.tab_layout = findViewById(R.id.tab_layout)
+        val fragments = Arrays.asList(TodayFragment(), WeekFragment())
+        adapter = FragmentPageAdapter(supportFragmentManager, ArrayList(fragments))
+        this.viewpager.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/firstapp/stdio/weatherapp/fragments/FragmentPageAdapter.kt
+++ app/src/main/java/com/example/firstapp/stdio/weatherapp/fragments/FragmentPageAdapter.kt
@@ -3,9 +3,9 @@ package com.example.firstapp.stdio.weatherapp.fragments
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import java.util.List
+import java.util.ArrayList
 
-class FragmentPageAdapter(fm: FragmentManager?, var fragment: List<Fragment>) : FragmentStatePagerAdapter(fm) {
+class FragmentPageAdapter(fm: FragmentManager?, var fragment: ArrayList<Fragment>) : FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         return fragment.get(position)
     }
